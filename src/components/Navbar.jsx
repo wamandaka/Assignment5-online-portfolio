447c7b40a15e25f35d5dc9ace72cbd45d97e56d4
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-10-01 07:53:07 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">deysiyuwai</a>
+          <a className="btn btn-ghost text-xl">deysi yuwai</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
