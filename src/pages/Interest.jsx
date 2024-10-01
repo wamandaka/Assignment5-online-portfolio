c94cb60c32fb466a10d087d5805642958cbdf894
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-10-01 07:52:59 +0700

    add content

--- src/pages/Interest.jsx
+++ src/pages/Interest.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Interest = () => {
-  return <div className="pt-20 px-4">Interest</div>;
+  return (
+    <div className=" px-4 md:px-10 h-screen flex flex-col justify-center items-center text-center">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold my-10">
+        INTEREST
+      </h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia
+      aspernatur ipsam fugiat velit. Eaque voluptatem ipsum, accusantium saepe
+      atque, at, exercitationem dolorum hic impedit rerum fugiat blanditiis
+      animi explicabo officiis ipsa quasi tempora dolore quia quam corporis.
+      Nisi modi, quis, saepe natus fugit tempora id ex perspiciatis ipsa ducimus
+      assumenda debitis magnam reiciendis. Odio, accusantium cum accusamus
+      nesciunt, delectus officia corrupti molestiae ratione deserunt debitis
+      officiis placeat iusto numquam expedita itaque commodi blanditiis ullam.
+      Nostrum blanditiis architecto quas sunt, voluptas ad asperiores sit?
+      Cupiditate, beatae ducimus impedit porro illum rerum et. Sint doloribus
+      rerum maxime pariatur itaque praesentium sunt!
+    </div>
+  );
 };
 
 export default Interest;
