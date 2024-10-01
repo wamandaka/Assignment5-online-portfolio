a36ed4a7f18072c930a0d667786ae6dcca1e0f62
wamandaka <108261088+wamandaka@users.noreply.github.com>
2024-10-01 07:52:50 +0700

    install react icon

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,31 +1,38 @@
 import React from "react";
+import { FaReact,FaJs,FaHtml5,FaCss3 } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
 
 const skills = [
   {
     id: 1,
     name: "React",
-    icon: "",
+    icon: <FaReact size={50}/>,
   },
   {
     id: 2,
     name: "Javascript",
-    icon: "",
+    icon: <FaJs size={50}/>,
   },
   {
     id: 3,
     name: "HTML",
-    icon: "",
+    icon: <FaHtml5 size={50}/>,
   },
   {
     id: 4,
     name: "CSS",
-    icon: "",
+    icon: <FaCss3 size={50}/>,
+  },
+  {
+    id: 5,
+    name: "Tailwind",
+    icon: <SiTailwindcss size={50}/>,
   },
 ];
 
 const Skills = () => {
   return (
-    <div className=" px-4 md:px-10 h-screen flex flex-col justify-center items-center text-center space-y-4">
+    <div className=" px-4 md:px-10 h-screen flex flex-col justify-center items-center text-center space-y-24 ">
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mt-10">
         SKILLS
       </h1>
@@ -35,8 +42,10 @@ const Skills = () => {
             className="avatar flex flex-col w-32 text-center items-center"
             key={skill.id}
           >
-            <div className="mask mask-hexagon w-24">
-              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            <div className="mask mask-hexagon w-24 bg-white">
+              <div className="flex justify-center items-center h-full">
+                {skill.icon}
+              </div>
             </div>
             <p className="text-2xl text-white">{skill.name}</p>
           </div>
